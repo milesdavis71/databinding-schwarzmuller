843828c20dd3c1ce680dc18fec9ea679a31b3b63
milesdavis71 <hevesiistvan71@hotmail.com>
2022-05-24 14:47:08 +0200

    05/064 Splitting Apps into Components - kész

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ServerElementComponent } from './server-element/server-element.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ServerElementComponent, CockpitComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
